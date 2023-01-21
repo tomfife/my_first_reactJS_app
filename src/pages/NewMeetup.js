fbb322ba3a8dd56723a9ce60ba27d8f7eeed5bd4
tomfife <71194993+tomfife@users.noreply.github.com>
2023-01-20 17:43:27 -0700

    Through 46 useEffect Hooks

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,31 @@
+import { useHistory } from 'react-router-dom';
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-    return <div>New Meetup Page</div>;
+    const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+        "https://react-demo-5dc21-default-rtdb.firebaseio.com/meetups.json",
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    ).then(() => {
+        history.replace('/');
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
